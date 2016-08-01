afd8ef58ecc88ffe28cd3f4322826765d19741b2
KemalC <ddeveloperr@gmail.com>
2016-08-01 17:36:39 +0000

    Simple search in the books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+     @books = Book.where(["name LIKE ?","%#{params[:search]}%"])
+    #@books = Book.all
   end
 
   # GET /books/1
