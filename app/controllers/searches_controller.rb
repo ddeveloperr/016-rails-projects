c2f2cce4f45f178580e09640a7898007fc9d54aa
KemalC <ddeveloperr@gmail.com>
2016-08-03 22:30:27 +0000

    Update the search form

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,24 +1,22 @@
 class SearchesController < ApplicationController
   # creates a new record in the "searches" table
-  def new
-	@search = Search.new
-  end
-  
-  # action creates a new path and redirects the user to that path
-  def create
-	@search = Search.create(search_params)
-	redirect_to @search
-  end
-  
-  # finds a user by its id
-  def show
-	@search = Search.find(params[:id])
-  end
+ 	def new
+	  @search = Search.new
+	  @category = Book.uniq.pluck(:category)
+	end
 
-  private
-  
-  # accesses the model's attributes and sends them as arguments to the create action
-  def search_params
+	def create
+	  @search = Search.create(search_params)
+	  redirect_to @search
+	end
+
+	def show
+	  @search = Search.find(params[:id])
+	end
+
+	private
+
+	def search_params
 	params.require(:search).permit(:keywords, :category, :min_price, :max_price, :isbn)
-  end
+	end
 end
